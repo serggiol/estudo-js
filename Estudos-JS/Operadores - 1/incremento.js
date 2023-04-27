6877617d6762d9e4b29f523afc81c2b1ef552079
Sérgio Luiz <sergiovidareal@gmail.com>
2023-04-27 18:21:30 -0300

    1.0

---
+++ Estudos-JS/Operadores - 1/incremento.js
@@ -0,0 +1,26 @@
+var x = 5
+console.log(x) // 5
+
+x = x - 1 //Diminui um
+console.log(x) // 4
+
+x-- //Diminui um
+console.log(x) // 3
+
+x = x + 1 //Adiciona um
+console.log(x) // 4
+
+x++ //Adiciona um
+console.log(x) // 5
+
+
+for(let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  
+/*
+Nesse caso, a variável i é iniciada com o valor 0 e, em cada iteração do loop, 
+o operador de incremento é usado para adicionar 1 ao seu valor. Então, na primeira iteração, 
+i vale 0, na segunda iteração i vale 1, e assim por diante, 
+até que o loop execute 5 vezes e i tenha o valor 4.
+*/
