faa8a010541acd73319c9f1c3b36fe60ef5ce4de
Sérgio Luiz <sergiovidareal@gmail.com>
2023-04-28 16:42:12 -0300

    condição

---
+++ Estudos-JS/Condições/if.js
@@ -0,0 +1,6 @@
+var velocidade = 12
+console.log(`Sua velocidade atual é de ${velocidade}Km/h`)
+if (velocidade > 60) {
+    console.log("Ultrapassou 60Km/h e você foi MULTADO!")
+}
+console.log("Ande sempre com o sinto de segurança!")
