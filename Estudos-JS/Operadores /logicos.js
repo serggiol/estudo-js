2b440ddc8561ff4dc85d4da8be9c79883a723273
Sérgio Luiz <sergiovidareal@gmail.com>
2023-05-08 15:53:57 -0300

    Delete Estudos-JS/Operadores  directory

--- Estudos-JS/Operadores /logicos.js
+++
@@ -1,7 +0,0 @@
-let x = 5; // Declaração de uma variável "x" com o valor 5
-let y = 10; // Declaração de uma variável "y" com o valor 10
-let z = "5"; // Declaração de uma variável "z" com o valor "5" como string
-
-console.log(x < y && x === 5); // O operador "&&" (AND) retorna "true" se ambos os operandos são verdadeiros. Nesse caso, retorna "true" porque "x" é menor que "y" e também é igual a 5.
-console.log(x < y || x === z); // O operador "||" (OR) retorna "true" se pelo menos um dos operandos é verdadeiro. Nesse caso, retorna "true" porque "x" é menor que "y" ou "x" é igual a "z".
-console.log(!(x === y)); // O operador "!" (NOT) nega o valor do operando. Nesse caso, retorna "true" porque "x" é diferente de "y", mas o operador "!" inverte o resultado final.
